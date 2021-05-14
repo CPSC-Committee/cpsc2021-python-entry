97729331d0647183f5daddb7a7083b3fc7c3d8d1
mato00 <mato00@163.com>
2021-05-14 12:56:17 +0800

    fixed condition in Line 143

--- score_2021.py
+++ score_2021.py
@@ -140,7 +140,7 @@ def score(data_path, ans_path):
 
         if len(endpoints_pred) == 0:
             class_pred = 0
-        elif len(endpoints_pred) == 1 and np.diff(endpoints_pred)[-1] == TrueRef.len_sig:
+        elif len(endpoints_pred) == 1 and np.diff(endpoints_pred)[-1] == TrueRef.len_sig - 1:
             class_pred = 2
         else:
             class_pred = 1
